b61cd51a597d0c2f07a571d2ca5d7c3f7c3b9f4d
pa7lux <pa7lux@gmail.com>
2023-05-06 17:18:12 +0400

    html-minifier

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const mediaquery = require('postcss-combine-media-query');
 const cssnano = require('cssnano');
+const htmlMinify = require('html-minifier');
 
 function serve() {
   browserSync.init({
@@ -17,8 +18,23 @@ function serve() {
 }
 
 function html() {
+  const options = {
+	  removeComments: true,
+	  removeRedundantAttributes: true,
+	  removeScriptTypeAttributes: true,
+	  removeStyleLinkTypeAttributes: true,
+	  sortClassName: true,
+	  useShortDoctype: true,
+	  collapseWhitespace: true,
+		minifyCSS: true,
+		keepClosingSlash: true
+	};
   return gulp.src('src/**/*.html')
         .pipe(plumber())
+        .on('data', function(file) {
+		      const buferFile = Buffer.from(htmlMinify.minify(file.contents.toString(), options))
+		      return file.contents = buferFile
+		    })
 				.pipe(gulp.dest('dist/'))
         .pipe(browserSync.reload({stream: true}));
 }
